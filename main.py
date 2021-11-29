ffa66f89ed6706ac98bee751a575e81b0c31c5c2
DSchmitz6320 <43940280+DSchmitz6320@users.noreply.github.com>
2021-11-28 19:34:19 -0500

    portfolio site
    A Flask-based portfolio site to showcase my final projects from Angela Yu's 100 Days of Code python programming course

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from flask import Flask, render_template, redirect, url_for, jsonify
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from datetime import date
 import requests
 
 
-GITHUB_TOKEN = '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'
+GITHUB_TOKEN = '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'
 GITHUB_USER = 'DSchmitz6320'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '47kJH438uihkjh438ohokjKJHle38'
+ckeditor = CKEditor(app)
 Bootstrap(app)
 
 def get_projects():
     url = f"https://api.github.com/users/{GITHUB_USER}/repos"
     headers = {"Accept":"application/vnd.github.mercy-preview+json"}
     repos = requests.get(url, headers=headers, auth=(GITHUB_USER,GITHUB_TOKEN)).json()
+    print(repos)
+    print('FUUUUUUUUUUUUUUUU')
     projects = []
     for repo in repos:
         if repo["homepage"]:
